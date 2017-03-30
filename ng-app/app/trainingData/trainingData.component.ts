d755f5e579a089e911cc4945b3768010134cec86
tamgalan <tamgalan@umich.edu>
2017-03-30 18:10:20 -0400

    s

--- ng-app/app/trainingData/trainingData.component.ts
+++ ng-app/app/trainingData/trainingData.component.ts
@@ -4,9 +4,10 @@ import { FormDataService }                       from '../data/formData.service'
 
 import { Dataset } from './dataset';
 
+import { User } from './user.interface';
 
 const DATASETS: Dataset[] = [
-    {id: 0, name:'UMICH', description: '273 users' },
+    {id: 0, name:'MICHIGAN', description: '273 users' },
     {id: 1, name:'BERKELEY', description: '100 patients'},
     {id: 2, name:'UCI', description: '1000 patients'},
     {id: 3, name:'STANDFOR', description: '2000 million patients'},
@@ -14,16 +15,39 @@ const DATASETS: Dataset[] = [
 ];
 
 
-
-
 @Component ({
-    selector:     'mt-wizard-training'
+    selector:     'mt-wizard-training',
+    styles: [`
+	h1 {
+	  color: yellow;
+	}
+	`]
     ,templateUrl: 'app/trainingData/trainingData.component.html'
 })
 
 export class trainingDataComponent implements OnInit, OnDestroy {
     title = 'Please select a training data set';
     datasets = DATASETS;
+
+    public user: User;
+
+    public genders = [
+        { value: 'F', display: 'Female' },
+        { value: 'M', display: 'Male' }
+    ];
+
+    public topics = [
+        { value: 'game', display: 'Gaming' },
+        { value: 'tech', display: 'Technology' },
+        { value: 'life', display: 'Lifestyle' },
+    ];
+
+    public toggles = [
+        { value: 'toggled', display: 'Toggled' },
+        { value: 'untoggled', display: 'UnToggled' },
+    ];
+
+
     selectedDataset: Dataset;
 
     onSelect(dataset: Dataset): void {
@@ -37,8 +61,16 @@ export class trainingDataComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
-        this.formData = this.formDataService.getData();
-        console.log('Training data feature loaded!');
+        // this.formData = this.formDataService.getData();
+        // console.log('Training data feature loaded!');
+        this.user = {
+            name: '',
+            gender: this.genders[0].value,
+            role: null,
+            isActive: false,
+            toggle: this.toggles[1].value,
+            topics: [this.topics[1].value]
+        }
     }
 
     ngOnDestroy() {
